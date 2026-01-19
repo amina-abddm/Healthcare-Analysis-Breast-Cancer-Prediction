9c2e10207efaff8678791ab4e3eb15a0c743a0ad
amina_abddm <amina.abd.75@gmail.com>
2026-01-19 19:25:46 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import joblib
 import numpy as np
+import os
 
-model = joblib.load("models/model_logistic_regression.pkl")
-scaler = joblib.load("models/scaler.pkl")
+# Dossier du fichier app.py
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-print("Bienvenue dans l'outil de prédiction du diagnostic cellulaire.")
+MODEL_PATH = os.path.join(BASE_DIR, "models", "logistic_regression_model.pkl")
+SCALER_PATH = os.path.join(BASE_DIR, "models", "standard_scaler.pkl")
+
+model = joblib.load(MODEL_PATH)
+scaler = joblib.load(SCALER_PATH)
+
+print("Bienvenue dans l'outil de prédiction du diagnostic du cancer du sein.")
 
 while True:
 
-    texture = float(input("Valeur de la texture de la cellule : "))
-    smoothness = float(input("Degré de régularité du contour cellulaire : "))
-    concave_points = float(input("Nombre de points de concavité sur le contour cellulaire : "))
-    symmetry = float(input("Degré de symétrie du contour cellulaire : "))
+    area_worst = float(input("Valeur de l'aire maximale de la cellule : "))
+    radius_worst = float(input("Valeur du rayon maximal de la cellule : "))
+    perimeter_worst = float(input("Valeur du périmètre maximal de la cellule : "))
+    concavity_mean = float(input("Valeur de la concavité moyenne de la cellule : "))
+    concave_points_mean = float(input("Valeur du nombre moyen de points concaves sur le contour cellulaire : "))
 
-    X_input = np.array([[texture, smoothness, concave_points, symmetry]])
+    X_input = np.array([[area_worst, radius_worst, perimeter_worst, concavity_mean, concave_points_mean]])
     X_scaled = scaler.transform(X_input)
 
     y_pred = model.predict(X_scaled)
